909f82eead02e90331561487a899c8805e532e42
Charles Liu <cliu2014@mit.edu>
2012-11-16 14:15:57 -0500

    Final version - command param error checking in main

--- main.cpp
+++ main.cpp
@@ -24,29 +24,45 @@ int main( int argc, char* argv[] )
   // "argument" provided to the program is actually the
   // name of the executable (in our case, "a4").
   for( int argNum = 1; argNum < argc; ++argNum ) {
-      cout << "Argument " << argNum << " is: " << argv[argNum] << std::endl;
+      cerr << "Argument " << argNum << " is: " << argv[argNum] << std::endl;
   }
   
   int index = 1;
-  char *inputFile, *outputFile = "output.bmp", *depthFile, *normalsFile;
+  char *inputFile, *outputFile = "output.bmp", *depthFile = "depth.bmp", *normalsFile = "normals.bmp";
   int sizeX = 200, sizeY = 200;
-  float depthMin, depthMax;
+  float depthMin = 8.0f, depthMax = 12.0f;
   bool doDepth = false, doNormals = false;
   while (index < argc) {
     if (strcmp("-input", argv[index]) == 0) {
       inputFile = argv[++index];
     } else if (strcmp("-size", argv[index]) == 0) {
-      sizeX = atoi(argv[++index]);
-      sizeY = atoi(argv[++index]);
+      if (index + 2 < argc) {
+        sizeX = atoi(argv[++index]);
+        sizeY = atoi(argv[++index]);
+      } else {
+        cerr << "ERROR: bad input with -size flag. Defaulting to 200x200." << endl;
+      }
     } else if (strcmp("-output", argv[index]) == 0) {
-      outputFile = argv[++index];
+      if (index + 1 < argc) {
+         outputFile = argv[++index];
+      } else {
+        cerr << "ERROR: bad input with -output flag. Defaulting to output.bmp." << endl;
+      }
     } else if (strcmp("-depth", argv[index]) == 0) {
-      depthMin = atof(argv[++index]);
-      depthMax = atof(argv[++index]);
-      depthFile = argv[++index];
+      if (index + 3 < argc) {
+        depthMin = atof(argv[++index]);
+        depthMax = atof(argv[++index]);
+        depthFile = argv[++index];
+      } else {
+        cerr << "ERROR: bad input with -depth flag. Defaulting to 8, 12, depth.bmp" << endl;
+      }
       doDepth = true;
     } else if (strcmp("-normals", argv[index]) == 0) {
-      normalsFile = argv[++index];
+      if (index + 1 < argc) {
+         normalsFile = argv[++index];
+      } else {
+        cerr << "ERROR: bad input with -normals flag. Defaulting to normals.bmp" << endl;
+      }
       doNormals = true;
     } else {
       cerr << "ERROR: bad command line arguments!";
